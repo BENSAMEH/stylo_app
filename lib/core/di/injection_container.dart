91376ccc964da21d62c90bfb8bd825cba47c14fe
mariam_alloush <mariamalosh98@gmail.com>
2026-07-13 05:56:49 +0300

    Add address API integration and profile updates

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -3,8 +3,11 @@ import 'package:get_it/get_it.dart';
 import '../api/api_client.dart';
 
 import '../../features/profile/data/datasource/profile_remote_datasource.dart';
+import '../../features/profile/data/datasource/address_remote_datasource.dart';
 import '../../features/profile/data/repositories/profile_repository_impl.dart';
 import '../../features/profile/presentation/cubit/profile_cubit.dart';
+import '../../features/profile/data/repositories/address_repository_impl.dart';
+import '../../features/profile/presentation/cubit/address/address_cubit.dart';
 
 final sl = GetIt.instance;
 
@@ -23,4 +26,15 @@ void setupDependencies() {
 
   // Profile Cubit
   sl.registerFactory(() => ProfileCubit(sl<ProfileRepositoryImpl>()));
+  // Address DataSource
+  sl.registerLazySingleton<AddressRemoteDatasource>(
+    () => AddressRemoteDatasource(sl<ApiClient>()),
+  );
+  // Address Repository
+  sl.registerLazySingleton<AddressRepositoryImpl>(
+    () => AddressRepositoryImpl(sl<AddressRemoteDatasource>()),
+  );
+
+  // Address Cubit
+  sl.registerFactory(() => AddressCubit(sl<AddressRepositoryImpl>()));
 }
